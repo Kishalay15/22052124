d95b5ef916e2a8058f7ce50eb56a10979b1f1c0f
Kishalay15 <kishalaylahiri@gmail.com>
2025-04-02 19:23:49 +0530

    added the test server url

--- utils/fetchHelper.js
+++ utils/fetchHelper.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const TEST_SERVER_URL = "https://test-social-media-api.com";
+const TEST_SERVER_URL = "http://20.244.56.144/evaluation-service";
 async function fetchData(endpoint) {
   try {
     const response = await axios.get(`${TEST_SERVER_URL}/${endpoint}`);
